ea5a3b6f6cd90119e6199c25fddfc6c79da66747
clee0809 <2017poplj@gmail.com>
2022-08-24 14:25:48 -0700

    settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -141,11 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # print(STATICFILES_DIRS)
 
 LOGIN_REDIRECT_URL = 'posts:all'  # 'test' # test.html
